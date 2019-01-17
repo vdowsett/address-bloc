39a79439aa4c36d366f2c67a73eaa1b243a301ac
vdowsett <vdowsett@gmail.com>
2019-01-17 16:30:46 -0600

    Creating Contacts Assignment

--- db/models/contact.js
+++ db/models/contact.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const Contact = sequelize.define('Contact', {
     name: DataTypes.STRING,
-    phone: DataTypes.STRING
+    phone: DataTypes.STRING,
+    email: DataTypes.STRING
   }, {});
   Contact.associate = function(models) {
     // associations can be defined here
